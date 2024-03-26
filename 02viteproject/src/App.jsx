af84096cb38dbc8c6bff3cd47c4b9a3d2d6b5f79
kmd-shafi <shaikshafi5259@gmail.com>
2024-03-26 14:37:21 +0530

    Hook-useState

---
+++ 02viteproject/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, Setcounter] = useState(15);
+
+  const addValue = () => {
+    if (counter != 20) {
+      counter = counter + 1;
+      Setcounter(counter);
+    }
+  };
+  const removeValue = () => {
+    if (counter != 0) {
+      counter = counter - 1;
+      Setcounter(counter);
+    }
+  };
+
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>couner value:{counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
